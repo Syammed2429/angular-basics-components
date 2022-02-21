b1af2de7014db9bd3d5f12d5db208c4f0e24a5fd
Dada Khalandar <kerry.blig12@gmail.com>
2022-02-21 12:20:31 +0530

    building success alert component

---
+++ src/app/SuccessAlert/successAlert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-successAlert',
+    templateUrl: './successAlert.component.html',
+    styles: [`
+    div {
+        color: green
+    }
+    `]
+})
+
+export class SuccessAlertComponent{}
